9081ed6f6920d25d8dd6c618124866a34317471a
Safiyya Sharif <SafiyyaSharif@lawn-128-61-5-198.lawn.gatech.edu>
2017-08-26 10:29:49 -0400

    db and config

---
+++ db/schema.sql
@@ -0,0 +1,12 @@
+CREATE DATABASE sequelized_burger;
+
+
+-- CREATE TABLE burgers 
+-- (
+-- 	id int NOT NULL AUTO_INCREMENT, 
+-- 	burger_name varchar(100) NOT NULL,
+-- 	devoured BOOLEAN DEFAULT false, 
+-- 	date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+-- 	PRIMARY KEY (id)
+
+-- );
---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Cheesy Charley', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('The Hangman', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('The Atomic Bomb', true);
+
